7554dc7edd47d12f5fd7abbf8f2ec0108d1fca76
maasyuki.tamate <masayuki.tamate.anago@gmail.com>
2025-09-29 14:34:50 +0900

    小窓表示１

--- GazoToolsTest005.py
+++ GazoToolsTest005.py
@@ -10,9 +10,13 @@ Created on 2025 09 29
 
 '''
 from lib.GazoToolsBasicLib import tkConvertWinSize
+import tkinter as tk
+from PIL import ImageTk, Image
+import time
 '''/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 クラス　メインデータ
 
+クラス　画像窓描画
 
 _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 '''
@@ -21,8 +25,54 @@ class HakoData():
         StartFolder = "c:"
         pass
 
+class GazoPicture():
+    def __init__(self):
+
+        self.StartFolder = "c:"
+        pass
+    def Drawing(self):
+        self.folder = ""
+        imageFolder = self.folder
+        img = Image.open("C:\\Teisyutubutu\\GazoTools\\data\\folder_32.png")
+        tkimg = ImageTk.PhotoImage(img)
+        width = tkimg.width()
+        height = tkimg.height()
+
+        Gazo = tk.Toplevel()
+        GazoCanvas = tk.Canvas(Gazo, width=width,height=height)
+        GazoCanvas.pack()
+        GazoCanvas.image = tkimg
+        GazoCanvas.create_image(0, 0, image=tkimg, anchor=tk.NW)
+        pass
+
+'''_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+メイン
+
+主窓初期化
+↓
+画像窓初期化
+↓
+画像窓表示
+↓
+入力系
+↓
+主窓機能
+↓
+メインループ（）
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+'''
 
 TKWINSIZEANDXY = tkConvertWinSize(list([200, 200, 400, 20]))
 TKWINSIZEANDXY = tkConvertWinSize(list([200, 200]))
 
-print(TKWINSIZEANDXY)
+root = tk.Tk()
+root.geometry(TKWINSIZEANDXY)
+
+
+Gazo = GazoPicture()#画像窓出した
+
+
+Gazo.Drawing()
+
+root.mainloop()
